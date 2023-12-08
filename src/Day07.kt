30351aa204ec69ccecd238e6cc8f44f06a77ba65
Anastasios Georgousakis <ageorgousakis@gmail.com>
2023-12-08 17:40:28 +0200

    day 7 puzzle

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,54 +1,56 @@
-val original_marks = "AKQJT"
-val ordered_marks = "EDCBA"
-val original_marks2 = "AKQTJ"
-val ordered_marks2 = "DCBA1"
-fun addType(hand: String) = powers2(hand).joinToString("").padEnd(5) + hand
-fun powers2(hand: String): List<Int> = hand.toList().sorted().reversed().fold(emptyMap<Char, Int>()) { acc, c ->
-    if (c == '1') {
-        val highestKeyPair = acc.maxByOrNull { it.value }
-        if (highestKeyPair == null) {
-            acc + (c to (acc.getOrDefault(c, 0) + 1))
-        } else {
-            acc + (highestKeyPair.key to highestKeyPair.value + 1)
-        }
-    } else
-        acc + (c to (acc.getOrDefault(c, 0) + 1))
-}.values.sorted().reversed()
-
-
 fun main() {
     val day = "07"
     val testPart1Result: Long = 6440
     val testPart2Result: Long = 5905
     val part1Result: Long = 246424613
     val part2Result: Long = 248256639
-    val showTestResult = true
+    val showTestResult = false
 
-    fun part1(input: List<String>): Long =
-        input.map {
+    fun scored(hand: String): String = hand.toList().sorted().reversed().fold(emptyMap<Char, Int>()) { acc, c ->
+        if (c == '1') {
+            val highestKeyPair = acc.maxByOrNull { it.value }
+            if (highestKeyPair == null) {
+                acc + (c to (acc.getOrDefault(c, 0) + 1))
+            } else {
+                acc + (highestKeyPair.key to highestKeyPair.value + 1)
+            }
+        } else
+            acc + (c to (acc.getOrDefault(c, 0) + 1))
+    }.values.sorted().reversed().joinToString("").padEnd(5) + hand
+
+    fun part1(input: List<String>): Long {
+        val original_marks = "AKQJT98765432"
+        val ordered_marks = "EDCBA98765432"
+        return input.map {
             val (hand, bid) = it.split(' ')
-            addType(hand.map {
-                if (!it.isDigit()) ordered_marks[original_marks.indexOf(it)] else it
-            }.joinToString("")) to bid.toLong()
+            val orderedHand = hand.map {
+                ordered_marks[original_marks.indexOf(it)]
+            }.joinToString("")
+            scored(orderedHand) to bid.toLong()
         }.sortedWith { a, b ->
             a.first.compareTo(b.first)
         }.mapIndexed { index, handToBid ->
-            println("${handToBid.first} to ${handToBid.second}")
+//            println("${handToBid.first} to ${handToBid.second}")
             handToBid.second * (index + 1)
         }.sum()
+    }
 
-    fun part2(input: List<String>): Long =
-        input.map {
+    fun part2(input: List<String>): Long {
+        val original_marks = "AKQTJ98765432"
+        val ordered_marks = "DCBA198765432"
+        return input.map {
             val (hand, bid) = it.split(" ")
-            addType(hand.map {
-                if (!it.isDigit()) ordered_marks2[original_marks2.indexOf(it)] else it
-            }.joinToString("")) to bid.toLong()
+            val orderedHand = hand.map {
+                ordered_marks[original_marks.indexOf(it)]
+            }.joinToString("")
+            scored(orderedHand) to bid.toLong()
         }.sortedWith { a, b ->
             a.first.compareTo(b.first)
         }.mapIndexed { index, handToBid ->
-            println("${handToBid.first} to ${handToBid.second}")
+//            println("${handToBid.first} to ${handToBid.second}")
             handToBid.second * (index + 1)
         }.sum()
+    }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day${day}_test")
