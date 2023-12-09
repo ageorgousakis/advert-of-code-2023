5625a7d70da84990573a6b9e96a2107c407f3460
Anastasios Georgousakis <ageorgousakis@gmail.com>
2023-12-09 17:49:39 +0200

    day 9 puzzle

--- src/Day09.kt
+++ src/Day09.kt
@@ -7,14 +7,14 @@ fun main() {
     val showTestResult = false
 
     fun part1(input: List<String>): Int = input.sumOf { line ->
-        val sequences = mutableListOf(line.split(' ').map(String::toInt))
+        val sequences = mutableListOf(line.split(' ').map { it.toInt() })
         while (sequences.last().any { it != 0 })
             sequences += sequences.last().windowed(2).map { (a, b) -> b - a }
         sequences.sumOf { it.last() }
     }
 
     fun part2(input: List<String>): Int = input.sumOf { line ->
-        val sequences = mutableListOf(line.split(' ').map(String::toInt))
+        val sequences = mutableListOf(line.split(' ').map { it.toInt() })
         while (sequences.last().any { it != 0 })
             sequences += sequences.last().windowed(2).map { (a, b) -> b - a }
         sequences.map { it.first() }.reversed().reduce { acc, i -> i - acc }
